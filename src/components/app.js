f231b7bfd9112ccc91b7a08eb1c3c07b86755e2c
Jace Babin <jbabin91@gmail.com>
2017-09-09 02:01:13 -0700

    Added axios to weather app to make http request

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 // Import containers
 import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
       <SearchBar />
-    );
+    )
   }
 }
