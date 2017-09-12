9d9bd68abdc727262a242beeec537ec31e96fc46
Jace Babin <jbabin91@gmail.com>
2017-09-12 15:28:18 -0700

    Connected fetchWeather function with the searchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,13 +1,19 @@
 'use strict'
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 
-export default class SearchBar extends Component {
+// Import actions
+import {fetchWeather} from '../actions/index'
+
+class SearchBar extends Component {
   constructor (props) {
     super(props)
 
     this.state = {term: ''}
 
     this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange (event) {
@@ -18,6 +24,8 @@ export default class SearchBar extends Component {
     event.preventDefault()
 
     // We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term)
+    this.setState({term: ''})
   }
 
   render () {
@@ -35,3 +43,10 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+function mapDispatchToProps (dispatch) {
+  // bind actions with redux
+  return bindActionCreators({fetchWeather}, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
