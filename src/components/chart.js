c341c069641e88f80df285f2de0844b2d30afbdc
Jace Babin <jbabin91@gmail.com>
2017-09-13 23:25:32 -0700

    Minor code clean-up

--- src/components/chart.js
+++ src/components/chart.js
@@ -4,7 +4,7 @@ import React from 'react'
 import {Sparklines, SparklinesLine, SparklinesReferenceLine} from 'react-sparklines'
 
 function average (data) {
-  return _.round(_.sum(data)/data.length)
+  return _.round(_.sum(data) / data.length)
 }
 
 export default (props) => {
