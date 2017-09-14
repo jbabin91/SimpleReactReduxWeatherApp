e6c4f1123ce2eabc8e464fce1a1bf73d401dbf11
Jace Babin <jbabin91@gmail.com>
2017-09-13 22:20:40 -0700

    Starting to build the weatherList container and display city names.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,6 +1,13 @@
 'use strict'
+import {FETCH_WEATHER} from '../actions/index'
 
-export default function (state = null, action) {
-  console.log('Action received: ', action)
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // These two lines are equivalent to each other.
+      // They both will concat the array state.
+      // return state.concat([action.payload.data])
+      return [action.payload.data, ...state]
+  }
   return state
 }
